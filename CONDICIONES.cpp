4a09fdc71269ef8c567d30c5603800392fd459f8
emireyes <141955808+emireyes@users.noreply.github.com>
2023-08-29 12:33:50 -0600

    Add files via upload

---
+++ CONDICIONES.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main (){
+	
+	int a=100;
+	if(!(a== 100))
+	{
+		printf("el valor es 10 \n");
+	}
+		else
+		 if(!(a==20))
+	{
+		printf("el valor es 20\n");
+	}
+		else
+	{
+		printf("no es 10 o 20\n"); 
+	}
+		
+	return 0;
+}
+
+
+
+
+
+//hacer un programa que nos haga u arbol de 3, 5 o 7 niveles, yo ponemos las condiciones /*
+
+			/*			*
+                      //|\\
+				    ////|\\\\
+			      //////|\\\\\\
+			          IIIII
+			          */
